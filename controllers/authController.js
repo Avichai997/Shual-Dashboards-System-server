22730cbf224dd17a070c9f7a91598dbbbb4315ce
Avichai Iluz <avichai997@gmail.com>
2022-08-08 11:43:45 +0300

    update login function

--- controllers/authController.js
+++ controllers/authController.js
@@ -70,7 +70,7 @@ exports.logout = (req, res) => {
     expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true
   });
-  res.status(200).json({ status: 'success' });
+  res.status(200).json({ status: 'success', token: null });
 };
 
 exports.protect = catchAsync(async (req, res, next) => {
